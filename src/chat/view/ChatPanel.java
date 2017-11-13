46ab6a7b54ec022830c2f9645095c7231f06bbce
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-11-13 13:56:05 -0700

    Created a ChatPanel for my chatbot

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,5 +1,20 @@
 package chat.view;
 
-public class ChatPanel {
+import javax.swing.JPanel;
+import gui.controller.ChatBotController;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ChatPanel extends JPanel
+{
+
+	private void setupPanel()
+	{
+
+	}
 
 }
