0e9cee63e49a0551dca2e7cb704870890c610238
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-11-13 13:42:14 -0700

    Created the ChatFrame for my chatbot.

---
+++ src/chat/view/ChatFrame.java
@@ -0,0 +1,26 @@
+package chat.view;
+
+import chat.controller.ChatbotController;
+import javax.swing.JFrame;
+
+public class ChatFrame extends JFrame
+{
+	private ChatbotController appController;
+	
+	public ChatFrame(ChatbotController appController)
+	{
+		super();
+		this.appController = appController;
+		appPane = new ChatPanel(appController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPane);
+		this.setTitle("Chatting with the chatbot");
+		this.setResizable(false);
+		this.setSize(600, 600);
+		this.setVisible(true);
+	}
+}
