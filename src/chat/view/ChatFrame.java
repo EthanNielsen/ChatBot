b4a4d43142db7f9e110bf5529e5f1927612fc413
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-11-15 12:22:43 -0700

    Commented out code that had errors in it.

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -6,18 +6,19 @@ import javax.swing.JFrame;
 public class ChatFrame extends JFrame
 {
 	private ChatbotController appController;
+//	private ChatPanel appPanel;
 	
 	public ChatFrame(ChatbotController appController)
 	{
 		super();
 		this.appController = appController;
-		appPane = new ChatPanel(appController);
+//		appPanel = new ChatPanel(appController);
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
-		this.setContentPane(appPane);
+//		this.setContentPane(appPanel);
 		this.setTitle("Chatting with the chatbot");
 		this.setResizable(false);
 		this.setSize(600, 600);
