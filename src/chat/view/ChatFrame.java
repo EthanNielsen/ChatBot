2dca42a38790a960b8ba680e468e2d613f3b833e
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-11-15 12:49:13 -0700

    Uncomented my code.

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -6,19 +6,19 @@ import javax.swing.JFrame;
 public class ChatFrame extends JFrame
 {
 	private ChatbotController appController;
-//	private ChatPanel appPanel;
+	private ChatPanel appPanel;
 	
 	public ChatFrame(ChatbotController appController)
 	{
 		super();
 		this.appController = appController;
-//		appPanel = new ChatPanel(appController);
+		appPanel = new ChatPanel(appController);
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
-//		this.setContentPane(appPanel);
+		this.setContentPane(appPanel);
 		this.setTitle("After nine years in development hopefully Chatbot will be worth the wait.");
 		this.setResizable(false);
 		this.setSize(600, 600);
