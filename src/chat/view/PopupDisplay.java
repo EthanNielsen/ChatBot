89d8624d6eeb15e9b6eb5db35302fff0b793ed49
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-11-17 12:11:11 -0700

    added documentation to PopupDisplay.

--- src/chat/view/PopupDisplay.java
+++ src/chat/view/PopupDisplay.java
@@ -8,17 +8,29 @@ public class PopupDisplay
 	private ImageIcon icon;
 	private String windowTitle;
 	
+	/**
+	 * Creates a Image when the app popups.
+	 */
 	public PopupDisplay()
 	{
 		icon = new ImageIcon(getClass().getResource("images/chatbot.png"));
 		windowTitle = "Chatot says";
 	}
 	
+	/**
+	 * 
+	 * @param message 
+	 */
 	public void displayText(String message)
 	{
 		JOptionPane.showMessageDialog(null, message, windowTitle, JOptionPane.INFORMATION_MESSAGE, icon);
 	}
 	
+	/**
+	 * This creates an answer when the user creates input.
+	 * @param question Is the question asked by the chatbot
+	 * @return Returns the answer created from the line of code.
+	 */
 	public String collectResponse(String question)
 	{
 		String answer = "";
