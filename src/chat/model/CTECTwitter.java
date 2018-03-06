e004b2b444243b73c56feaadfe3148d76b1fa507
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-03-06 13:04:39 -0700

    added nessisary imports to CTECHTwitter.java

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -7,7 +7,17 @@ import chat.controller.ChatbotController;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.Twitter;
+import twitter4j.GeoLocation;
+import twitter4j.Paging;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.ResponseList;
+import twitter4j.Status;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.text.DecimalFormat;
 
 public class CTECTwitter 
 {
