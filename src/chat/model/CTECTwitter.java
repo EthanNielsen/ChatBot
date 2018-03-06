5e3169dc526307284df06f48f2a4d61560445bbe
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-03-06 12:43:43 -0700

    deeleted reinstated variables

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,28 +1,33 @@
 package chat.model;
 
+import java.io.File;
+
 import chat.controller.ChatbotController;
+import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-import twitter4j.Twitter;
-import twitter4j.Status;
 
 
 public class CTECTwitter 
 {
 	private ChatbotController appController;
 	private Twitter chatbotTwitter;
+	private List<Status> searchedTweets;
+	private List<String> tweetedWords;
+	private long totalWordCount;
 	
 	public CTECTwitter(ChatbotController appController)
 	{
 		this.appController = appController;
 		this.chatbotTwitter = TwitterFactory.getSingleton();
 	}
+	
 
 	public void sendTweet(String textToTweet)
 	{
 		try
 		{
-			chatbotTwitter.updateStatus(textToTweet + "@ChatbotCTEC");
+			chatbotTwitter.updateStatus(textToTweet + " @ChatbotCTEC");
 		}
 		catch(TwitterException tweetError)
 		{
