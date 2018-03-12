2c9b0b8a3c5b10b5975e4ac742b9cc830f0817fb
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-03-12 13:27:54 -0600

    added the message about the most common word in the getMostCommonWord method.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,8 +1,10 @@
 package chat.model;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import chat.controller.ChatbotController;
@@ -58,8 +60,24 @@ public class CTECTwitter
 		totalWordCount = tweetedWords.size();
 		String [] boring = createIgnoredWordArray();
 		trimTheBoringWords(boring);
+		removeBlanks();
+		generateWordCount();
 		
-		return mostCommon;
+		ArrayList<Map.Entry<String, Integer>> sorted = sortHashMap();
+		
+		String mostCommonWord = sorted.get(0).getKey();
+		int maxWord = 0;
+		
+		maxWord = sorted.get(0).getValue();
+		
+		mostCommon = "The most common word in " + username + "'s "+ searchedTweets.size() + " tweets is " + 
+		mostCommonWord + ", and it was used " + maxWord + " times.\nThis is " + 
+				(DecimalFormat.getPercentInstance().format(((double) maxWord)/totalWordCount)) + 
+				" of total words: " + totalWordCount + " and is " + 
+				(DecimalFormat.getPercentInstance().format(((double) maxWord/wordsAndCount.size())) + 
+						" of the unique words; " + wordsAndCount.size() ;
+ 
+ 		return mostCommon;
 	}
 
 	private void collectTweets(String username)
